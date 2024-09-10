777500a882b53a5c1ce79462211fb03817cae3e2
nignanfatao1 <nignanfatao170@gmail.com>
2024-09-10 16:29:15 +0000

    Create testcmd.js

---
+++ commandes/testcmd.js
@@ -0,0 +1,16 @@
+buttons: [
+            {
+                "name": "cta_url",
+                "buttonParamsJson": `{\"display_text\":\"Instagram 🦋\",\"url\":\"https://instagram.com/${Config.instagramId}\",\"merchant_url\":\"https://www.google.com\"}`
+            },
+            {
+                "name": "cta_url",
+                "buttonParamsJson": "{\"display_text\":\"WhatsApp Channel 🔮\",\"url\":\"https://whatsapp.com/channel/0029VaDIPkA6buMS9hRE7y2G\",\"merchant_url\":\"https://www.google.com\"}"
+            },
+            {
+                "name": "cta_url",
+                "buttonParamsJson": "{\"display_text\":\"YouTube Channel 💗\",\"url\":\"https://youtube.com/@pika_kunn\",\"merchant_url\":\"https://www.google.com\"}"
+            },
+            { "name": "quick_reply", "buttonParamsJson": `{\"display_text\":\"Owner 👤\",\"id\":\"${prefix}owner\"}` },
+            { "name": "quick_reply", "buttonParamsJson": `{\"display_text\":\"Script 🧿\",\"id\":\"${prefix}sc\"}` }
+        ],
